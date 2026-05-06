290dd0ee480d142434619174dcf3049abd9d9cf0
Raul <mansurovraul1@gmail.com>
2026-05-06 08:19:34 +0300

    Fix version to 2.0.0

--- StackTrace/src/python/setup.py
+++ StackTrace/src/python/setup.py
@@ -38,7 +38,7 @@ ext_modules = [
 
 setup(
     name="stacktrace",
-    version="1.0.0",
+    version="2.0.0",
     author="Raul Mansurov",
     author_email="mansurovraul1@gmail.com",
     description="Low-level Windows Stack Trace Engine",
